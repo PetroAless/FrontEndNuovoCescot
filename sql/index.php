05064ff4c572edafb4f37f73c530baed8bd815b3
PetroAless <alessio.petrotta1@gmail.com>
2023-03-02 14:47:00 +0100

    index.php loading a query, but needs a fix, need to understand how and why i need to use placeholders

--- sql/index.php
+++ sql/index.php
@@ -1,3 +1,16 @@
+<?php 
+    require "library.php";
+    function statement($pdoTmp, $columnS, $contents){ //insert into passing a pdo instance and strings for variables
+        $sql = "INSERT INTO ppl $columnS VALUES (:contents)"; //$column is the name of the column it's gonna fill and contents
+        $statement = $pdoTmp->prepare($sql);                   //is what is gonna go in
+        //                                                                  fix?
+        $statement->execute([
+            ':contents' => $contents
+            
+        ]);
+        
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +20,28 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>TRY OF SQL + PHP</h1>
     <?php 
-    
+        echo "<p>php works</p>";
+        $connStr="mysql:host=$host;dbname=$db;charset=UTF8";
+        try {
+            //                                                                  NEED A FIX
+            //                                                                  NEED A FIX
+            //                                                                  NEED A FIX
+            //                                                                  NEED A FIX
+            //                                                                  NEED A FIX
+            $pdo = new PDO($connStr,$username,$password);
+            $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if($pdo)echo "Connection to the db: \"$db\" successful! <br>";
+            statement($pdo,"(firstName, lastName)","'Alessio', 'Petrotta'");
+            /*$statement = $pdo ->prepare("INSERT INTO ppl (firstName,lastName) VALUES ('Alessio','Petrotta');");
+            $nome = "Alessio";
+            $cognome = "Petrotta";
+            $statement ->execute([]);*/
+        } catch (PDOException $e) {
+            echo "<br>AAAAA PDO EXCEPTION <br>";
+            echo $e->getMessage();
+        }
     ?>
 </body>
 </html>
