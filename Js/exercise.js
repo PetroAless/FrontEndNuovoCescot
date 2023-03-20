c0d6f6554e7b29656636a4cafd11eb864b1d63c2
PetroAless <alessio.petrotta1@gmail.com>
2023-03-20 12:54:20 +0100

    aggiunte funzionalità: -div numerati con numero corretto -creadiv e set class funzioni -2 button

--- Js/exercise.js
+++ Js/exercise.js
@@ -1,30 +1,34 @@
-function backColor(){
-    let dvs = document.querySelectorAll(".importante");
-    dvs.forEach(el => {
-        el.style.backgroundColor = "lightgrey";
-    });
-}
-
-function js(){
-    let btn = document.createElement("button");
-    btn.setAttribute("id","trigger");
-    btn.innerText="avvia";
-    btn.addEventListener("click",backColor);
-    document.body.append(btn);
-        
-    let cls = document.createAttribute("class");
-    cls.value="importante";
-    
+var divCount = 0;
+function creaDiv(){
     let dvn;
     let pn;
     let str1="div numero ";
     for(let i=0;i<10;i++){
         dvn = document.createElement("div");
-        pn = document.createElement("p");
-        dvn.setAttribute("class","importante");
-        pn.innerHTML=str1+i;
         document.body.append(dvn);
+        pn = document.createElement("p");
+        pn.innerHTML=str1+divCount;
         dvn.appendChild(pn);
+        divCount++;
     }
+}
+function setClass(){
+    let dvn = document.getElementsByTagName("div");;
+    for(let i=0;i<dvn.length;i++){
+        dvn[i].classList.add("importante");
+    }
+}
+function js(){
+    let btn = document.createElement("button");
+    btn.setAttribute("id","trigger");
+    btn.innerText="crea div";
+    btn.addEventListener("click",creaDiv);
+    document.body.append(btn);
+
+    btn = document.createElement("button");
+    btn.innerText="set classe";
+    btn.addEventListener("click",setClass);
+    document.body.append(btn);
+
 }
 addEventListener("DOMContentLoaded",js);
