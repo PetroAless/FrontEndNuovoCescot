804c0aa5a368c447d8fd5c54e7f6b6f71d4cf74f
PetroAless <alessio.petrotta1@gmail.com>
2023-03-20 11:40:50 +0100

    added DOMContentLoaded event plus more readable code

--- Js/script.js
+++ Js/script.js
@@ -8,20 +8,23 @@ function smallerFont(){
     font-=10;
     p1.style.fontSize=font+"px";
 }
-let p1 = document.querySelector("#p1");
+function create_buttons(){
+    let bigB = document.createElement("button");
+    let smallB = document.createElement("button");
+    document.body.prepend(smallB);
+    document.body.prepend(bigB);
+    bigB.innerHTML="font+";
+    smallB.innerHTML="font-";
+    
+    bigB.addEventListener("click",biggerFont);
+    smallB.addEventListener("click",smallerFont);
+}
 
-p1.innerHTML="beh questo è un p";
+function startJs(){
+    let p1 = document.querySelector("#p1");
+    create_buttons();
+}
 
-
-let bigB = document.createElement("button");
-let smallB = document.createElement("button");
-document.body.prepend(smallB);
-document.body.prepend(bigB);
-bigB.innerHTML="font+";
-smallB.innerHTML="font-";
-
-bigB.addEventListener("click",biggerFont);
-smallB.addEventListener("click",smallerFont);
-
+document.addEventListener("DOMContentLoaded",startJs);
 
 
